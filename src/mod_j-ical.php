d34a5ffc37c336259d8c6a2141849cc3c4555bf4
Jokr97 <36475029+Jokr97@users.noreply.github.com>
2022-02-07 21:54:29 +0100

    boilerplate for joomla-module added

---
+++ src/mod_j-ical.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author     Lukas Schneider
+ * @license    GNU GENERAL PUBLIC LICENSE Version 3; see LICENSE
+ * @link       https://github.com/Jokr97
+ */
+
+use Joomla\CMS\Helper\ModuleHelper;
+
+defined('_JEXEC') or die;
+
+$moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
+
+require ModuleHelper::getLayoutPath('mod_foo', $params->get('layout', 'default'));
